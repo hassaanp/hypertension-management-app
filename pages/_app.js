a180ef0a4a01ac4608c9ef3722893ec788362944
Hassaan Pasha <pasha.hassaan@gmail.com>
2022-10-30 18:16:51 +0500

    added firebase and hosting

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,12 @@
 import '../styles/globals.css'
 
+import { doc, getFirestore } from 'firebase/firestore';
+import { FirebaseAppProvider, FirestoreProvider, useFirestoreDocData, useFirestore, useFirebaseApp } from 'reactfire';
+
+const firebaseConfig = {
+  /* Add in your config object from the Firebase console */
+};
+
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
