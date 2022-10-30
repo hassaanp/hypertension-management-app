9a9f623a0583e3a59fa69c3cd2f0303f4eb4a624
Hassaan Pasha <pasha.hassaan@gmail.com>
2022-10-30 19:08:07 +0500

    added main screen

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,29 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
 
-import { doc, getFirestore } from 'firebase/firestore';
-import { FirebaseAppProvider, FirestoreProvider, useFirestoreDocData, useFirestore, useFirebaseApp } from 'reactfire';
+import { doc, getFirestore } from "firebase/firestore";
+import {
+  FirebaseAppProvider,
+  FirestoreProvider,
+  useFirestoreDocData,
+  useFirestore,
+  useFirebaseApp,
+} from "reactfire";
 
 const firebaseConfig = {
-  /* Add in your config object from the Firebase console */
+  apiKey: "AIzaSyAUSC_-gT3-1iPdgYjgNIizFJzX6lGaQog",
+  authDomain: "hypertension-management.firebaseapp.com",
+  projectId: "hypertension-management",
+  storageBucket: "hypertension-management.appspot.com",
+  messagingSenderId: "945134787891",
+  appId: "1:945134787891:web:8e274ea02ff96e27317246",
 };
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <FirebaseAppProvider firebaseConfig={firebaseConfig}>
+      <Component {...pageProps} />
+    </FirebaseAppProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
